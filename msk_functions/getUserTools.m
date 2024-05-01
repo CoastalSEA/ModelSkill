bc398501ff848efbddd987f9ee73f4cab0a2c60b
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2024-05-01 16:52:31 +0000

    Added dependent properties for Wmtl etc to CSTrunmodel.m Continued to port functionality for analysis methods to EnergyFlux from original version. Corrected call for subdomain in getUserTools
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/ModelSkill@259 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- msk_functions/getUserTools.m
+++ msk_functions/getUserTools.m
@@ -182,11 +182,10 @@ function hf = getRhythmicForm(mobj)
     title(titletxt)    
 end
 %%
-function SD = getRhythmicSpacing(obj,grid,zdiff,casedesc)
+function SD = getRhythmicSpacing(~,grid,zdiff,casedesc)
     %compute the rhythmic form spacing and amplitude
     SD = [];
     %create subgrid selection plot
-%     [subdomain,sublimitxt] = getSubDomain(obj,grid);
     [subdomain,sublimitxt] = gd_subdomain(grid);
     if isempty(subdomain), return; end
     casedesc = sprintf('Subgrid of %s\nUsing %s',casedesc,sublimitxt);
